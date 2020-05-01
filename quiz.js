8fdc8de1ebce84ebc9198f6d57b0b7fdd9203175
Elmatador <kmouad29@gmail.com>
2020-04-30 18:15:53 -0600

    trying to push to github

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-// select all elements
+//create variables
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var question = document.getElementById("question");
